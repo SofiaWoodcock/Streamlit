10b6e9aa2a821e00909d0b263a8cadcb6940a165
SofiaWoodcock <89831275+SofiaWoodcock@users.noreply.github.com>
2021-10-11 22:36:26 +0700

    Update mysl.py

--- mysl.py
+++ mysl.py
@@ -35,8 +35,16 @@ with row1_1:
     date_selected = st.selectbox("Date of January,2019",("1", "2","3","4","5"))
     hour_selected = st.slider("Select hour", 0, 23)
     
-date_selected["1"] = ("https://raw.githubusercontent.com/Maplub/odsample/master/20190101.csv")
-date_selected["2"] = ("https://raw.githubusercontent.com/Maplub/odsample/master/20190102.csv")
+if date_selected == "1" :
+  DATA_URL = ("https://raw.githubusercontent.com/Maplub/odsample/master/20190101.csv")
+elif date_selected == "2" :
+  DATA_URL = ("https://raw.githubusercontent.com/Maplub/odsample/master/20190102.csv")
+elif date_selected == "3" :
+  DATA_URL = ("https://raw.githubusercontent.com/Maplub/odsample/master/20190103.csv")
+elif date_selected == "4" :
+  DATA_URL = ("https://raw.githubusercontent.com/Maplub/odsample/master/20190104.csv")
+elif date_selected == "5" :
+  DATA_URL = ("https://raw.githubusercontent.com/Maplub/odsample/master/20190105.csv")
 
 @st.cache(persist=True)
 def load_data(nrows):
